63d9d62fa22064d1a46015833029303fb25dd690
TadevosKurdoglian <tazman311@gmail.com>
2021-07-08 08:41:04 +0300

    first commit

---
+++ TwoRecanglesApp1/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TwoRecanglesApp1
+//
+//  Created by Тадевос Курдоглян on 07.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
